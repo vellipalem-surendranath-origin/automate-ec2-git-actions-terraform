dc1ff84fb1bd2a8f9fe42bf343cfb7c3e318d6f6
surendranatha reddy vellipalem <vellips@surendranathas-MacBook-Air.local>
2024-09-19 21:18:38 -0500

    removing duplicates in outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,10 +3,6 @@ output "ec2_public_ip" {
   value = aws_instance.medusa_instance.public_ip
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.example.public_ip
-}
-
 output "ec2_ssh_user" {
   value = "ec2-user"  # Replace with the appropriate user for your AMI
 }
