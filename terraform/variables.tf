022dadf0b5446e51708a2273cc8497144a6cc732
surendranatha reddy vellipalem <vellips@surendranathas-MacBook-Air.local>
2024-09-19 21:20:45 -0500

    add missing variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,8 @@ variable "ec2_key_pair" {
   type        = string
   default     = "medusa-ec2-terraform"
 }
+
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  default     = "ap-south-1"
+}
